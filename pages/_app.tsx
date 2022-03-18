5c518a4aa51ebf3b672970f9ac9f2398bbd6b100
Yousof <yousof@synthesis.ai>
2022-03-19 00:58:14 +0300

    Updates

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import '../styles/theme-minimal.scss'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
--- services/loaders.ts
+++
@@ -1,5 +0,0 @@
-import { linkPrefix } from "./run-time";
-
-export function imageLoader({ src } : {src: string}) {
-  return `${linkPrefix}/img/${src}`; 
-}
